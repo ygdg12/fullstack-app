e49474e868b640969c3bc03a821bb07ffdecc8d4
Yared <yaregirmab1234@gmail.com>
2025-08-16 09:56:02 +0300

    Update axios base URL

--- frontend1/src/lib/axios.js
+++ frontend1/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
-export const axiosInstance= axios.create({
-    baseURL:"http://localhost:5001/api",
-      withCredentials:true,
-   
-})
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE === "development"
+        ? "http://localhost:5001/api"
+        : "https://fullstack-app-sxv1.onrender.com/api", // deployed backend URL
+    withCredentials: true,
+});
