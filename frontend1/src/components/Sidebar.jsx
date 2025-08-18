002a44dd0e7a69fe635db84b3af24da258839538
Yared <yaregirmab1234@gmail.com>
2025-08-18 09:59:14 +0300

    sider6

--- frontend1/src/components/Sidebar.jsx
+++ frontend1/src/components/Sidebar.jsx
@@ -74,7 +74,7 @@ const Sidebar = () => {
                             )}
                         </div>
 
-                        <div className="hidden lg:block text-left min-w-0">
+                        <div className="text-left min-w-0">
                             <div className="font-medium truncate">{user.fullName}</div>
                             <div className="text-sm text-zinc-400">
                                 {onlineUsers.includes(user._id) ? "Online" : "Offline"}
